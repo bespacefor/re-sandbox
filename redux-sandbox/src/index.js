baf6f3430a5b72a1b21d963d2d2f294b088002fc
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-11-28 23:53:24 +0300

    refactor: change structure of the project

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,44 +1,26 @@
 import { createStore } from 'redux';
-
-const reducer = (state = 0, action) => {
-
-  switch (action.type) {
-    case 'DEC':
-      return state - 1;
-
-    case 'INC':
-      return state + 1;
-
-    case 'RND':
-      return state + action.payload;
-
-    default:
-      return state;
-  }
-};
+import reducer from './reducer';
+import { dec, inc, rnd } from './actions'
 
 const store = createStore(reducer);
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'DEC' });
+    store.dispatch(dec());
   });
 
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'INC' });
+    store.dispatch(inc());
   });
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch({
-      type: 'RND',
-      payload
-    });
+    store.dispatch(rnd(payload));
   });
 
 const update = () => {
