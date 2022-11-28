9bbef8c613106a08591707ac9592c71e55c8168f
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-11-29 02:21:24 +0300

    refactor: use mapDispatchToProps as object

--- redux-sandbox/src/actions.js
+++ redux-sandbox/src/actions.js
@@ -1,3 +1,8 @@
 export const dec = () => ({ type: 'DEC' });
 export const inc = () => ({ type: 'INC' });
-export const rnd = (payload) => ({ type: 'RND', payload });
+export const rnd = () => {
+    return {
+        type: 'RND',
+        payload: Math.floor(Math.random() * 10)
+    };
+};
